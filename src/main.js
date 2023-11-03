bf9be43e3c3654cb4345f016b5828fe2691eea99
pfcs33 <aaapfcs@gmail.com>
2023-11-03 19:04:50 +0800

    add sass as css preprocessor

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import "element-plus/dist/index.css";
-import "./style.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 
@@ -8,10 +7,12 @@ import store from "./store/index.js";
 
 import BaseCard from "./components/ui/BaseCard.vue";
 import BaseButton from "./components/ui/BaseButton.vue";
+import SvgIcon from "./components/ui/SvgIcon.vue";
 
 const app = createApp(App);
 app.use(router);
 app.use(store);
 app.component("BaseCard", BaseCard);
 app.component("BaseButton", BaseButton);
+app.component("SvgIcon", SvgIcon);
 app.mount("#app");
